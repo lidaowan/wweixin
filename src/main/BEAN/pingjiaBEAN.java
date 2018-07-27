eeb39e019508eb5b8d3714fe18d7293a1f27b4a8
lidaowan <516480168@qq.com>
2018-07-27 17:49:22 +0800

    2018年7月27日 17:49:05

---
+++ src/main/BEAN/pingjiaBEAN.java
@@ -0,0 +1,66 @@
+public class pingjiaBEAN {
+
+    int id;
+    int good_id;
+    String openid;
+    String pingjiashijian;
+    String neirong;
+    String shifouzhanshi;
+    String order_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGood_id() {
+        return good_id;
+    }
+
+    public void setGood_id(int good_id) {
+        this.good_id = good_id;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getPingjiashijian() {
+        return pingjiashijian;
+    }
+
+    public void setPingjiashijian(String pingjiashijian) {
+        this.pingjiashijian = pingjiashijian;
+    }
+
+    public String getNeirong() {
+        return neirong;
+    }
+
+    public void setNeirong(String neirong) {
+        this.neirong = neirong;
+    }
+
+    public String getShifouzhanshi() {
+        return shifouzhanshi;
+    }
+
+    public void setShifouzhanshi(String shifouzhanshi) {
+        this.shifouzhanshi = shifouzhanshi;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+}
